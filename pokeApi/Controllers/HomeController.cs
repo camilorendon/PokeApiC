38f3b2d045758a4cce980c0ce5763972b8a0d589
camilorendon <juancamilo.g.0512@gmail.com>
2022-11-05 12:46:33 -0500

    initial commit

---
+++ pokeApi/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using pokeApi.Models;
+using System.Diagnostics;
+
+namespace pokeApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            //https://pokeapi.co/api/v2/pokemon/
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("https://pokeapi.co/api/v2/pokemon/");
+            var result = JsonConvert.DeserializeObject(json);
+            return View(result);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
